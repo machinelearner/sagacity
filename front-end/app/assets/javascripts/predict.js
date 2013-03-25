2068c6f3fafb54c15116d342a85fd3a5efbb46c3
Pramod N <npramod05@gmail.com>
2013-03-25 07:18:45 +0530

    Css addition

--- front-end/app/assets/javascripts/predict.js
+++ front-end/app/assets/javascripts/predict.js
@@ -1,5 +1,6 @@
 $(function(){
     $('#generate_sentiment').click(function(){
+            $('#sentiment_value').html("");
         $.post('predict', $('#tweet_text').serialize(),function(data) {
             $('#sentiment_value').html(data.class);
         });
